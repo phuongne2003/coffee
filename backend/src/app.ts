a829e9d9b673ce4631d5f82083ab697db2092e32
Vo Huu Thang <vohuuthang36@gmail.com>
2026-04-14 12:34:45 +0700

    fix(cors): configure CORS in app.ts to allow frontend requests

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,6 +6,28 @@ import { swaggerSpec } from "./swagger/swagger";
 
 const app = express();
 
+const allowedOrigins = new Set([
+	"http://localhost:5173",
+	"http://127.0.0.1:5173",
+]);
+
+app.use((req, res, next) => {
+	const origin = req.headers.origin;
+
+	if (!origin || allowedOrigins.has(origin)) {
+		res.header("Access-Control-Allow-Origin", origin ?? "*");
+		res.header("Access-Control-Allow-Credentials", "true");
+		res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+		res.header("Access-Control-Allow-Methods", "GET,POST,PATCH,PUT,DELETE,OPTIONS");
+	}
+
+	if (req.method === "OPTIONS") {
+		return res.sendStatus(204);
+	}
+
+	return next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
